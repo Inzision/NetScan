355af8711aefa840973b5134d645733a82bfe0fe
Inzision <jbmbjbmb@hotmail.ca>
2017-04-23 22:33:19 -0700

    Update Basic Packet Inspection.py

--- Basic Packet Inspection.py
+++ Basic Packet Inspection.py
@@ -1,29 +1,37 @@
+from scapy.all import *
+from dpkt.compat import compat_ord
 import dpkt
 import socket
 import datetime
 
-f = open("mycap.pcap")
-pcap = dpkt.pca.Reader(f)
-for ts, buf in pcap:
-	try:
-		eth = dpkt.ethernet.Ethernet(buf)
-		ip = eth.data
-		src = socket.inet_ntoa(ip.src)
-		dst = socket.inet_ntoa(ip.dst)
-		if ip.p == dpkt.ip.IP_PROTO_TCP:
-			tcp = ip.data
+def mac_addr(address):#converts mac address from hex to a readable format
+	return ':'.join('%02x' % compat_ord(b) for b in address)
+
+packets = sniff(iface = "eth0",filter="tcp and port 80",count=50) #captures 50 tcp port 80 ports from interface specified
+wrpcap("new.pcap",packets)#writes capture packets toa pcap file called new.pcap
+
+f = open("new.pcap")
+pcap = dpkt.pcap.Reader(f)
+
+try:
+	for ts, buf in pcap:
+		eth = dpkt.ethernet.Ethernet(buf)#unpacks ethernet frame
+		ip = eth.data# grabs data from ethernet frame(ip packet)
+		src = socket.inet_ntoa(ip.src)#uses socket to grab source ip from ip packet
+		dst = socket.inet_ntoa(ip.dst)#uses socket to grab destination ip from ip packet
+		print ("Source IP: %s \tDestination IP: %s" %(src,dst))
+		print ("Source MAC: %s \tDestination MAC: %s"%(mac_addr(eth.src),mac_addr(eth.dst)))#puts through funtion and prints out the mac addresses
+		print ("")
+		if ip.p == dpkt.ip.IP_PROTO_TCP:#checks to see if the ip packet is using TCP
+			tcp = ip.data#grabs the data from the ip packet(TCP Fragment)and assigns it to tcp
 			print ("TCP PACKET")
-			print ("Source Port: %s \tDestination Port: %s" %(tcp.sport,tcp.dsport))
-		elif ip.p == dpkt.ip.IP_PROTO_UDP:
-			udp = ip.data
+			print ("Source Port: %s \t\tDestination Port: %s" %(tcp.sport,tcp.dport))
+		elif ip.p == dpkt.ip.IP_PROTO_UDP:#checks to see if the ip packet is using UDP
+			udp = ip.data#grabs the data from the ip packet(UDP Fragment) and assigns it to udp
 			print ("UDP PACKET")
-			print ("Source Port: %s \tDestination Port: %s" %(udp.sport,udp.dsport))
+			print ("Source Port: %s \t\tDestination Port: %s" %(udp.sport,udp.dport))
 		else:
 			pass
-		print ("Source IP: %s \tDestination IP: %s" %(src,dst))
-		print ("Source MAC: %s \tDestination MAC: %s" %(eth.src,eth.dst))
-		print ("Time: %s" %(str(datetime.datetime.utcfromtimestamp(ts))))
-		print ("")
-	except:
-		pass
+except:
+	pass
 f.close()
